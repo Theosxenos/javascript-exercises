12302cf809ed7f132faa266b0159436701465329
TheosXenos <blueonion@proton.me>
2023-02-01 11:52:45 +0100

    Finish Exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string)
+{
+    let stringlength = string.length;
+    let reversedsring = "";
+    for(let i = 1; i <= stringlength; i++)
+    {
+        reversedsring += string.substr(-i, 1);
+    }
 
+    return reversedsring;
 };
 
 // Do not edit below this line
