c33f82384e03376ec58067aec8b4d31976e33603
TheosXenos <blueonion@proton.me>
2023-02-01 12:12:13 +0100

    Finish Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for(let arg of args)
+    {
+        let argindex = array.indexOf(arg);
+        if(argindex >= 0)
+        {
+            array.splice(argindex, 1);
+        }
+    }
 
+    return array;
 };
 
 // Do not edit below this line
