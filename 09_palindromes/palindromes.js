be3a93882c8166990a2cce5343c5ba119add663e
TheosXenos <64856229+Theosxenos@users.noreply.github.com>
2023-02-11 12:56:36 +0100

    Finish Exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (palindrome) {
+    const regex = /[^a-zA-Z]/g;
+    let strpalstripped = palindrome.replaceAll(regex,"");
+    let strpallowered = strpalstripped.toLowerCase().split('');
+    let strpallreversed = [...strpallowered].reverse();
 
+    strpallowered = strpallowered.join('');
+    strpallreversed = strpallreversed.join('');
+
+    return strpallowered == strpallreversed;
 };
 
 // Do not edit below this line
