1de77d44e5e10a1df0e5dd74d1937f556214ee9a
TheosXenos <64856229+Theosxenos@users.noreply.github.com>
2023-02-11 13:01:52 +0100

    Refactor exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,10 +1,9 @@
 const palindromes = function (palindrome) {
     const regex = /[^a-zA-Z]/g;
     let strpalstripped = palindrome.replaceAll(regex,"");
-    let strpallowered = strpalstripped.toLowerCase().split('');
-    let strpallreversed = [...strpallowered].reverse();
+    let strpallowered = strpalstripped.toLowerCase();
+    let strpallreversed = strpallowered.split('').reverse();
 
-    strpallowered = strpallowered.join('');
     strpallreversed = strpallreversed.join('');
 
     return strpallowered == strpallreversed;
