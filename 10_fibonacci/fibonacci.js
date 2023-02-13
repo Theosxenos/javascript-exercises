a3aa7a24598f15be8cc3621598a36bb499a9ac04
TheosXenos <64856229+Theosxenos@users.noreply.github.com>
2023-02-13 10:00:14 +0100

    Complete Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    if(Number(x) == "NaN")
+    {
+        return;
+    }
 
+    if(x <= 0)
+    {
+        return "OOPS";
+    }
+
+    let fibarr = [] ;
+    fibarr[0] = 0;
+    fibarr[1] = 1;
+
+
+    for(let i = 2; i <= x; i++)
+    {
+        fibarr[i] = fibarr[i-1] + fibarr[i-2];
+    }
+
+    return fibarr[x];
 };
 
 // Do not edit below this line
