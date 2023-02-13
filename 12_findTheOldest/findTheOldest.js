2b52716e8c4f26ab62cdbbe38c8386652d38d20d
TheosXenos <64856229+Theosxenos@users.noreply.github.com>
2023-02-13 10:50:03 +0100

    Add "Teacher solution"

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,23 +1,35 @@
-const findTheOldest = function(people) {
+const findTheOldest = function (people) {
     let oldest = people[0];
 
-    for(let p of people)
-    {
-        if(p.yearOfDeath === undefined)
-        {
+    for (let p of people) {
+        if (p.yearOfDeath === undefined) {
             p.yearOfDeath = (new Date()).getFullYear();
         }
 
         let page = p.yearOfDeath - p.yearOfBirth;
         let oage = oldest.yearOfDeath - oldest.yearOfBirth;
 
-        if(page > oage)
-        {
+        if (page > oage) {
             oldest = p;
         }
     }
 
     return oldest;
+};
+
+const findTheOldest2 = (people) => {
+    people.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge < currentAge ? currentPerson : oldest;
+    });
+};
+
+const getAge = function (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 };
 
 // Do not edit below this line
