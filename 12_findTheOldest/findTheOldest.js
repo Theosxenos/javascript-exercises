60fc3bf22b2375e4e047640c77af219405a2d20f
TheosXenos <64856229+Theosxenos@users.noreply.github.com>
2023-02-13 10:45:29 +0100

    Finish Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = people[0];
 
+    for(let p of people)
+    {
+        if(p.yearOfDeath === undefined)
+        {
+            p.yearOfDeath = (new Date()).getFullYear();
+        }
+
+        let page = p.yearOfDeath - p.yearOfBirth;
+        let oage = oldest.yearOfDeath - oldest.yearOfBirth;
+
+        if(page > oage)
+        {
+            oldest = p;
+        }
+    }
+
+    return oldest;
 };
 
 // Do not edit below this line
